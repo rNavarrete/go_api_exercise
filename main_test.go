2b084dc15178811a1e4d7c69b6558e489011ec9a
Rolando Navarrete <rolandonavarrete@Rolandos-MacBook-Pro.local>
2018-06-30 20:03:34 -0700

    added a create user test

--- main_test.go
+++ main_test.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"bytes"
+	"encoding/json"
 	"log"
 	"net/http"
 	"net/http/httptest"
@@ -40,6 +42,54 @@ func TestEmptyTable(t *testing.T) {
 
 	if body := response.Body.String(); body != "[]" {
 		t.Errorf("Expected an empty array. Got %s", body)
+	}
+}
+
+func TestCreateUser(t *testing.T) {
+	clearTable()
+	// create a payload with fake user data
+	payload := []byte(`{"name": "test user", "age": 30}`)
+	// create a POST request to the /user endpoint and attach our userdata payload
+	req, _ := http.NewRequest("POST", "/user", bytes.NewBuffer(payload))
+	// execute the request and capture the request
+	response := executeRequest(req)
+	// make sure that the response code is a a successful one (201)
+	checkResponseCode(t, http.StatusCreated, response.Code)
+	// create a map variable
+	var m map[string]interface{}
+	// unmarshal parses the json-encoded data and stores the result in the value pointed to by v
+	json.Unmarshal(response.Body.Bytes(), &m)
+
+	if m["name"] != "test user" {
+		t.Errorf("Expected user name to be `test user`. Got `%v'", m["name"])
+	}
+	if m["age"] != "30" {
+		t.Errorf("Expected user age to be `30`. Got `%v'", m["age"])
+	}
+	// the id is compared to 1.0 because JSON unmarshaling converts numbers to
+	//floats, when the target is a map[string]interface{}
+	if m["id"] != 1.0 {
+		t.Errorf("Expected user ID to be `1`. Got `%v`", m["id"])
+	}
+}
+
+// The following test, tests two things, the status code 404 and if the reponse contains the expected error message
+func TestGetNonExistentUser(t *testing.T) {
+	// clear the users table
+	clearTable()
+	// create a request for user 45 on the users table
+	req, _ := http.NewRequest("GET", "/user/45", nil)
+	// execute the request and capture the result
+	response := executeRequest(req)
+	// make sure that the result is a status not found one
+	checkResponseCode(t, http.StatusNotFound, response.Code)
+	// create an empty map
+	var m map[string]string
+	// parses the encoded json and stores the value in the second variable
+	json.Unmarshal(response.Body.Bytes(), &m)
+	// fail the test if the error is not what is expected.
+	if m["error"] != "User not found" {
+		t.Errorf("Expected the error key of the response to be set to `User not found`. Got `%s`", m["error"])
 	}
 }
 
